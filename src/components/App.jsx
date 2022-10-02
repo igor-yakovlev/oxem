28f726467be5634b7fd78fa441851ce4e731b1c3
Igor <igor95-2010@mail.ru>
2022-10-02 16:05:29 +0500

    refactored input range component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,66 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import InputRange from './InputRange';
-import {useState} from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
+import InputRange from './InputRange'
+import { useState } from 'react'
 
 function App() {
-
-  const [price, setPrice] = useState(1000000);
-  const [initiaProcentlFee, setInitialProcentFee] = useState(10);
-  const [term, setTern] = useState(10);
+  const [price, setPrice] = useState(1000000)
+  const [initialProcentFee, setInitialProcentFee] = useState(10)
+  const [term, setTerm] = useState(1)
 
   const getInitialFee = () => {
-    return Math.ceil((initiaProcentlFee * 100) / price);
+    return Math.ceil((initialProcentFee * 100) / price)
   }
 
   const handleChangePrice = (price) => {
-    setPrice(price);
+    setPrice(price)
   }
 
   const handleChangeInitialProcentFee = (initialFee) => {
-    setInitialProcentFee(initialFee);
+    setInitialProcentFee(initialFee)
+  }
+
+  const handleChangeTerm = (term) => {
+    setTerm(term)
   }
 
   return (
-    <Container className="p-0 m-5">
-      <Row className="mt-5">
+    <Container className='p-0 m-5'>
+      <Row className='mt-5'>
         <Col md={'6'}>
           <h1 className='title'>Рассчитайте стоимость автомобиля в лизинг</h1>
         </Col>
       </Row>
       <Row>
         <Col md={'4'}>
-          <InputRange data={'₽'} min={1000000} max={6000000} label={'Стоимость автомобиля'} value={price} onChange={handleChangePrice} />
+          <InputRange
+            data={'₽'}
+            step={100000}
+            min={1000000}
+            max={6000000}
+            label={'Стоимость автомобиля'}
+            value={price}
+            onChange={handleChangePrice}
+          />
         </Col>
         <Col md={'4'}>
-          <InputRange data={`${initiaProcentlFee}%`} min={10} max={60} label={'Первоначальный взнос'} value={initiaProcentlFee} onChange={handleChangeInitialProcentFee} />
+          <InputRange
+            data={`${initialProcentFee}%`}
+            min={10}
+            max={60}
+            label={'Первоначальный взнос'}
+            value={initialProcentFee}
+            onChange={handleChangeInitialProcentFee}
+          />
         </Col>
         <Col md={'4'}>
-          <InputRange data={'мес.'} min={10} max={60} label={'Срок лизинга'} value={term} onChange={setTern}/>
+          <InputRange
+            data={'мес.'}
+            min={1}
+            max={60}
+            label={'Срок лизинга'}
+            value={term}
+            onChange={handleChangeTerm}
+          />
         </Col>
       </Row>
     </Container>
--- src/components/InputRange.jsx
+++ src/components/InputRange.jsx
@@ -1,38 +1,44 @@
-import { React, useState } from 'react';
-import styles from './InputRange.module.scss';
+import { React, useState, useEffect } from 'react'
+import styles from './InputRange.module.scss'
 
-const InputRange = ({ data, min, max, label, onChange, value }) => {
-
+const InputRange = ({ data, min, max, label, onChange, value, step = 1 }) => {
+  
   const getBackgroundSize = () => {
-    return { backgroundSize: `${(value * 100) / max}% 100%` };
-  };
+    value = Number(toPlainString(value))
+    value = checkLimits(value, min, max)
+    return { backgroundSize: `${((value - min) * 100) / (max - min)}% 100%` }
+  }
+
+  const handleBlur = ({ target }) => {
+    const { value } = target
+    const digVal = Number(toPlainString(value))
+    const checkedVal = checkLimits(digVal, min, max)
+    onChange(checkedVal)
+  }
 
   const handleChange = ({ target }) => {
-    const { value } = target;
-    if (value === '' || +value < 0) {
-      onChange(min);
-    } else {
-      onChange(value);
-    }
-  };
-
+    const { value } = target
+    onChange(value)
+  }
   return (
     <>
-      <label className={styles.input__label} htmlFor={data}>{label}</label>
+      <label className={styles.input__label} htmlFor={data}>
+        {label}
+      </label>
       <div className={styles.input}>
         <input
           className={styles.input__text}
           id={data}
-          type='number'
-          value={value}
+          type='text'
+          value={numberWithSpaces(value)}
+          onBlur={handleBlur}
           onChange={handleChange}
         />
-        <span className={styles.input__data}>
-          {data}
-        </span>
+        <span className={styles.input__data}>{data}</span>
         <div className={styles.range_container}>
           <input
-            type="range"
+            type='range'
+            step={step}
             min={min}
             max={max}
             className={styles.input__range}
@@ -43,9 +49,25 @@ const InputRange = ({ data, min, max, label, onChange, value }) => {
         </div>
       </div>
     </>
-
   )
 }
 
+function numberWithSpaces(value) {
+  const plain = toPlainString(value)
+  return plain.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+}
 
-export default InputRange;
+function toPlainString(str) {
+  return str.toString().replace(/\s/g, '')
+}
+
+function checkLimits(value, min, max) {
+  if (value > min && value < max) return value
+  if (value <= min) {
+    return min
+  } else {
+    return max
+  }
+}
+
+export default InputRange
